c94793305aadc474144f6a384ea8d9295d59c828
Mage0Foxy <91876215+Mage0Foxy@users.noreply.github.com>
2022-04-07 10:11:59 +0200

    Update MainPage.cs
    This should have the read fucntion somewaht working?

--- FoxFinder/MainPage.cs
+++ FoxFinder/MainPage.cs
@@ -12,9 +12,30 @@ namespace FoxFinder
 {
     public partial class MainPage : UserControl
     {
+        MongoClient m_Client;
+        IMongoDatabase m_Database;
+        IMongoCollection<Media> m_Collection;
+        
         public MainPage()
         {
             InitializeComponent();
+            m_Client = new MongoClient("mongodb+srv://NovaFoxy:Kyuubi99@maincluster.jtr1q.mongodb.net/FoxFinder?retryWrites=true&w=majority");
+            m_Database = m_Client.GetDatabase("FoxFinder");
+            m_Collection = m_Database.GetCollection<Media>("Media");
+        }
+        //Refresh Button, This asks to MongoDB get colletion (Name from Collection) <Media> (Find data [place => x.Name record == "Name") Show value First(We need show all value here)
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ding = m_Database.GetCollection<Media>("Media").FindSync(x => x.Media_type == "Game").FirstOrDefault();
+                MessageBox.Show(ding.Media_title, ding.Media_playable);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Yo");
+            }
         }
 
     }
